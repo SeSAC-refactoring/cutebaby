e5506b071136eb18e6071c4087fd60df9ee9f5fc
Yundo-git <ydh7804@gmail.com>
2025-01-31 15:37:06 +0900

    email Login backup

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 // import { fetchUsers, UserDataset } from './services/userService';
-import LoginTest from './pages/LoginTest';
 
 import { Route,BrowserRouter, Routes } from 'react-router-dom';
 import TestMain from './pages/TestMain';
@@ -38,84 +37,18 @@ export interface UserData {
 }
 
 function App() {
-    // 데이터의 타입을 UserData 배열로 설정
-    // const [data, setData] = useState<UserDataset[]>([]);
 
-    // useEffect(() => {
-    //     axios
-    //         .post<UserDataset[]>('http://localhost:5001/api/user/getUser')
-    //         .then((response) => {
-    //             setData(response.data);
-    //         })
-    //         .catch((error) => {
-    //             console.error('데이터 가져오기 오류:', error);
-    //         });
-
-    //////////////////////////////////////////////////////////
-    // API 데이터 가져오기 확인
-    // fetchVaccinationDiseaseList().then((list) => {
-    //     console.log(list);
-    // });
-    // fetchVaccinationInfo(10).then((list) => {
-    //     console.log(list);
-    // });
-
-    // fetchProvince().then((list) => {
-    //     console.log(list);
-    // });
-    // fetchCity(1100000000).then((list) => {
-    //     console.log(list);
-    // });
-    // const pagenumber = 1;
-    // const province = 1100000000;
-    // const city = 11110;
-    // const searchType = 'ORG'; // 'ADDR'
-    // const searchWord = '서울';
-    // fetchVaccinationCenters(
-    //     pagenumber,
-    //     province,
-    //     city,
-    //     searchType,
-    //     searchWord
-    // ).then((list) => {
-    //     console.log('병원정보', list);
-    // });
-
-    // fetchGrowthChartLms().then((list) => {
-    //     console.log('fetchGrowthChartLms', list);
-    // });
-    // fetchGrowthChartPercentile().then((list) => {
-    //     console.log('fetchGrowthChartPercentile', list);
-    // });
-    //////////////////////////////////////////////////////////
-    // }, []);
 
   return (
     <div className="App">
       <h1>MySQL 데이터 test</h1>
       <hr />
-      {/* <ul>
-                {data.map((item, index) => (
-                    <li key={index}>{JSON.stringify(item)}</li>
-                ))}
-            </ul> */}
-      <LoginTest />
-      <hr />
-      <EmailLogin></EmailLogin>
-      <hr />
-      {/* <Posttest /> */}
-      <a href={getKakaoLoginUrl()}>
-        <img src="img/kakaoLoginImg.png" alt="카카오 로그인" />
-      </a>
-      <KakaoLogin></KakaoLogin>
-
-      {/* 페이지 확인 */}
-      <hr />
-      {/* <VaccinationPage></VaccinationPage> */}
-      <GrowthDiaryPage></GrowthDiaryPage>
-
-      <hr />
-      {/* <Signup></Signup> */}
+      <Routes>
+                <Route path="/" element={<EmailLogin />} />
+                <Route path="/TestMain" element={<TestMain />} />
+                </Routes>
+    
+      <Signup></Signup>
 
       {/* <hr />
       <Login />
