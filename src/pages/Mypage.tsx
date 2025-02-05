4d997b931cdfd432c60f52758336072916977ec7
baejuhyang”git config --global user.email juhyang9897@gmail.com” <juhyang9897@gmail.com>
2025-02-05 11:20:59 +0900

    경로 수정

--- src/pages/Mypage.tsx
+++ src/pages/Mypage.tsx
@@ -1,67 +1,70 @@
-import { useEffect, useState } from "react";
-import { BabyList} from "../components/My-page/BabyList";
-import styles from "../styles/Mypage.module.scss";
-import {NeedLoginModal} from "../components/NeedLoginModal"; // 모달 import
+import { useEffect, useState } from 'react';
+import { BabyList } from '../components/my-page/BabyList';
+import styles from '../styles/Mypage.module.scss';
+import { NeedLoginModal } from '../components/NeedLoginModal'; // 모달 import
 import axios from 'axios';
-import { log } from "console";
+import { log } from 'console';
 
-interface babyinfo{
-  babyname : string,
-  birthday : string,
-  gender : string,
-  picture: string | null
+interface babyinfo {
+    babyname: string;
+    birthday: string;
+    gender: string;
+    picture: string | null;
 }
 
 export default function Mypage() {
+    const user = JSON.parse(sessionStorage.getItem('usernumber') || '{}'); // 세션에 담긴거가져오기
+    const [babyInfo, setBabyInfo] = useState<babyinfo[]>([]); // 여러 아기 정보를 다룰 수 있도록 배열로 초기화
+    const [nothingBaby, setNothing] = useState<boolean>(false);
+    //모달관리
+    const [openModal, setOpenModal] = useState(false);
+    //페이지 시작할때 세션확인
+    useEffect(() => {
+        const user = sessionStorage.getItem('user'); // 세션에 담긴거가져오기
+        if (!user) {
+            // 비었다면 밑에 실행
+            setOpenModal(true);
+        }
+    }, []);
 
-  const user = JSON.parse(sessionStorage.getItem("usernumber")||"{}"); // 세션에 담긴거가져오기
-  const [babyInfo, setBabyInfo] = useState<babyinfo[]>([]); // 여러 아기 정보를 다룰 수 있도록 배열로 초기화
-  const [nothingBaby , setNothing] = useState<boolean>(false)
-  //모달관리
-  const [openModal, setOpenModal] = useState(false);
-  //페이지 시작할때 세션확인
-  useEffect(() => {
-    const user = sessionStorage.getItem("user"); // 세션에 담긴거가져오기
-    if (!user) { // 비었다면 밑에 실행
-      setOpenModal(true);
-    }
-  }, []);
+    useEffect(() => {
+        babycall();
+    }, []);
 
-  useEffect(()=>{
-    babycall()
-},[])
+    const babycall = async () => {
+        const response = await axios.post(
+            'http://localhost:5001/api/babyinfo',
+            { user }
+        );
+        console.log('qwerqwer', response);
 
-  const babycall =async ()=>{
-    const response =  await axios.post("http://localhost:5001/api/babyinfo",{user});
-    console.log('qwerqwer',response);
-    
-    if(!response){
-      setNothing(false)
-      console.log('asdfsadf');
-      
-    }else{
-      console.log('babycall',response.data)
-      setNothing(true)
-      setBabyInfo(response.data)
-    }
-  
-}
-  return (
-    <div className={styles.wrap}>
-      {/* openMoal이 트루면 모달 실행 */}
-      {openModal && <NeedLoginModal modalState={() => setOpenModal(false)} />} 
+        if (!response) {
+            setNothing(false);
+            console.log('asdfsadf');
+        } else {
+            console.log('babycall', response.data);
+            setNothing(true);
+            setBabyInfo(response.data);
+        }
+    };
+    return (
+        <div className={styles.wrap}>
+            {/* openMoal이 트루면 모달 실행 */}
+            {openModal && (
+                <NeedLoginModal modalState={() => setOpenModal(false)} />
+            )}
 
-      <div className={styles.pageContent}>
-        {/* <div className={styles.icon}>수정 아이콘</div> */}
-        <div className={styles.background}>
-          <h1 className={styles.title}>마이페이지</h1>
-          <BabyList babyInfo={babyInfo} nothingBaby={nothingBaby} />
-          {/* {message.trim()
+            <div className={styles.pageContent}>
+                {/* <div className={styles.icon}>수정 아이콘</div> */}
+                <div className={styles.background}>
+                    <h1 className={styles.title}>마이페이지</h1>
+                    <BabyList babyInfo={babyInfo} nothingBaby={nothingBaby} />
+                    {/* {message.trim()
           ?
           :<BabyInput message={message} />} */}
+                </div>
+                <div className={styles.invisible}></div>
+            </div>
         </div>
-        <div className={styles.invisible}></div>
-      </div>
-    </div>
-  );
+    );
 }
