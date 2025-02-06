1490ad9c96efea363855321653e028b44b5142da
yujin08 <kkkyu28@naver.com>
2025-02-06 14:15:25 +0900

    css 작업중

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,52 +1,40 @@
-import { AiChatComponent } from '../components/home-page/AiChatComponent';
-import styles from '../styles/Home.module.scss';
-import { useDispatch, useSelector } from 'react-redux';
-import { AppDispatch, RootState } from '../store';
-
+import { AiChatComponent } from "../components/home-page/AiChatComponent";
+import styles from "../styles/Home.module.scss";
+import { useDispatch, useSelector } from "react-redux";
+import { AppDispatch, RootState } from "../store";
 
 export default function Home() {
-    const dispatch = useDispatch<AppDispatch>();
-    const { babyInfo } = useSelector(
-        (state: RootState) => state.baby
-    );
+  const dispatch = useDispatch<AppDispatch>();
+  const { babyInfo } = useSelector((state: RootState) => state.baby);
 
-    console.log('로그인성공시 babyinfo불러옴',babyInfo)
+  console.log("로그인성공시 babyinfo불러옴", babyInfo);
 
-    return (
-        <>
-            <div className={styles.background}>
-                <div className={styles.block_user}>
-                    <div className={styles.big_title}>
-                        땡땡이님, 안녕하세요?
-                    </div>
-                    <div className={styles.profile}></div>
-                    <div className={styles.list_box}></div>
-                </div>
-                <div className={styles.block_center}>
-                    <div>
-                        <div className={styles.small_title}>
-                            다가오는 예방접종
-                        </div>
-                        <div className={styles.vac_box}></div>
-                    </div>
-                    <div>
-                        <div className={styles.small_title}>최근 예방접종</div>
-                        <div className={styles.vac_box}></div>
-                    </div>
-                    <div>
-                        <div className={styles.small_title}>우리 땡땡이는?</div>
-                        <div className={styles.user_info}></div>
-                    </div>
-                    <div className={styles.banner_wrap}>
-                        <div className={styles.banner}></div>
-                        <div className={styles.banner}></div>
-                    </div>
-                </div>
-                <div className={styles.block_chatbot}>
-                    <div className={styles.chatbot}>도움이 필요하신가요?</div>
-                    <AiChatComponent />
-                </div>
+  return (
+    <>
+      <div className={styles.background}>
+        <div className={styles.block_user}>
+          <div className={styles.big_title}>
+            땡땡이님,
+            <br />
+            안녕하세요
+          </div>
+          <div className={styles.small_title}>다가오는 예방접종</div>
+          <div className={styles.small_box}></div>
+          <div className={styles.big_box}></div>
+          <div className={styles.banner_wrap}>
+            <div className={styles.banner}>
+              <div className={styles.small_title}>예방점종 상세설명</div>
             </div>
-        </>
-    );
+            <div className={styles.banner}>
+              <div className={styles.small_title}>위탁의료기관 찾기</div>
+            </div>
+          </div>
+        </div>
+        <div className={styles.block_chatbot}>
+          <div className={styles.chatbot}>도움이 필요하신가요?</div>
+          <AiChatComponent />
+        </div>
+      </div>
+    </>
+  );
 }
