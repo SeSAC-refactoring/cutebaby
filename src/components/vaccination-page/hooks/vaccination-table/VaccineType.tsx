3a04043f76c00f17c0f6b8bf504a67ec84cb3108
yujin08 <kkkyu28@naver.com>
2025-02-08 04:19:24 +0900

    css 작업중

--- src/components/vaccination-page/hooks/vaccination-table/VaccineType.tsx
+++ src/components/vaccination-page/hooks/vaccination-table/VaccineType.tsx
@@ -1,37 +1,65 @@
-import React from 'react';
+import React from "react";
 
 export const VaccineType: React.FC = () => {
-    const vaccines = [
-        'HepB',
-        'BCG(피내용)',
-        'DTaP',
-        'Tdap/Td',
-        'IPV',
-        'Hib',
-        'PCV',
-        'PPSV',
-        'RV1',
-        'RV5',
-        'MMR',
-        'VAR',
-        'HepA',
-        'LJEV(불활성화 백신)',
-        'LJEV(약독화 생백신)',
-        'HPV',
-        'IIV',
-    ];
+  const vaccines = [
+    "HepB",
+    "BCG(피내용)",
+    "DTaP",
+    "Tdap/Td",
+    "IPV",
+    "Hib",
+    "PCV",
+    "PPSV",
+    "RV1",
+    "RV5",
+    "MMR",
+    "VAR",
+    "HepA",
+    "LJEV(불활성화 백신)",
+    "LJEV(약독화 생백신)",
+    "HPV",
+    "IIV",
+  ];
 
-    return (
-        <div>
-            <div>백신 종류 및 방법</div>
-            <ul style={{ listStyle: 'none', padding: '0', margin: '0' }}>
-                {vaccines.map((vaccine, i) => (
-                    <li key={i}>
-                        <span>{vaccine}</span>
-                        <span>{i + 1}</span>
-                    </li>
-                ))}
-            </ul>
-        </div>
-    );
+  return (
+    <div>
+      <div
+        style={{
+          width: "179px",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          height: "56px",
+          backgroundColor: "#93CBDF",
+          fontSize: "22px",
+          fontWeight: "bold",
+          borderRight: "3px solid #E1E1E5",
+          boxSizing: "border-box",
+        }}
+      >
+        백신 종류 및 방법
+      </div>
+      <ul style={{ listStyle: "none", padding: "0", margin: "0" }}>
+        {vaccines.map((vaccine, i) => (
+          <li
+            key={i}
+            style={{
+              whiteSpace: "pre-line", // \n을 인식하여 줄바꿈 적용
+              width: "179px",
+              height: i === 10 ? "100px" : "50px",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              borderBottom: "2px solid #E1E1E5",
+              borderRight: "3px solid #E1E1E5",
+              boxSizing: "border-box",
+            }}
+          >
+            <span>{vaccine}</span>
+            <span>{i + 1}</span>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
--- src/pages/VaccinationCenters.tsx
+++ src/pages/VaccinationCenters.tsx
@@ -64,67 +64,63 @@ export default function VaccinationCenters() {
           <p>데이터를 불러오는 중...</p>
         </div>
       )}
+      <div className={styles.search_wrap}>
+        <h2>위탁의료기관 찾기</h2>
 
-      <h2>위탁의료기관 찾기</h2>
+        {/* 지역(시/도) 선택 드롭다운 */}
+        <div className={styles.select_wrap}>
+          <select
+            ref={refs.province}
+            value={selectedLocation.province}
+            disabled={isFirstLoading} // 데이터 불러오는 동안은 드롭다운으로 지역 선택 불가
+            onChange={handleProvinceSelect}
+            className={styles.select}
+          >
+            <option value="">시/도</option>
+            {provinces.map((province) => (
+              <option key={province.cd} value={province.cd}>
+                {province.cdNm}
+              </option>
+            ))}
+          </select>
 
-      {/* 지역(시/도) 선택 드롭다운 */}
-      <select
-        ref={refs.province}
-        value={selectedLocation.province}
-        disabled={isFirstLoading} // 데이터 불러오는 동안은 드롭다운으로 지역 선택 불가
-        onChange={handleProvinceSelect}
-        className={styles.select}
-      >
-        <option value="">시/도</option>
-        {provinces.map((province) => (
-          <option key={province.cd} value={province.cd}>
-            {province.cdNm}
-          </option>
-        ))}
-      </select>
+          {/* 도시(시/군/구) 선택 드롭다운 (지역이 선택되지 않으면 disabled) */}
+          <select
+            ref={refs.city}
+            value={selectedLocation.city}
+            disabled={!selectedLocation.province}
+            onChange={handleCitySelect}
+            className={styles.select}
+          >
+            <option value="">시/군/구</option>
+            {cities.map((city) => (
+              <option key={city.cd} value={city.cd}>
+                {city.cdNm}
+              </option>
+            ))}
+          </select>
 
-      {/* 도시(시/군/구) 선택 드롭다운 (지역이 선택되지 않으면 disabled) */}
-      <select
-        ref={refs.city}
-        value={selectedLocation.city}
-        disabled={!selectedLocation.province}
-        onChange={handleCitySelect}
-        className={styles.select}
-      >
-        <option value="">시/군/구</option>
-        {cities.map((city) => (
-          <option key={city.cd} value={city.cd}>
-            {city.cdNm}
-          </option>
-        ))}
-      </select>
+          <input
+            className={styles.select}
+            type="text"
+            value={inputAddress}
+            placeholder="주소"
+            onChange={(e) => setInputAddress(e.target.value)}
+            onKeyDown={(e) => {
+              if (e.key === "Enter") {
+                handleSearch();
+              }
+            }}
+          />
 
-      <input
-        className={styles.select}
-        type="text"
-        value={inputAddress}
-        placeholder="주소"
-        onChange={(e) => setInputAddress(e.target.value)}
-        onKeyDown={(e) => {
-          if (e.key === "Enter") {
-            handleSearch();
-          }
-        }}
-      />
-
-      {/* 검색 버튼 */}
-      <button onClick={handleSearch}>병원 검색</button>
-      <VaccinationUnit />
-      {/* <CenterList 
-      {
-        centers: Centers[],
-    toggleVaccineList : toggleVaccineList,
-    showVaccineList: { showVaccineList };
-      }
-      /> */}
+          {/* 검색 버튼 */}
+          <button className={styles.search_btn} onClick={handleSearch}>
+            검색
+          </button>
+        </div>
+      </div>
       {/* 검색 결과(병원 리스트) 표시 */}
-      <div>
-        <h3>병원 목록</h3>
+      <div className={styles.contents_wrap}>
         {!hasSearched.current ? (
           <p>병원을 찾아보세요</p> // 검색 전 표시 문구
         ) : isLoading ? (
