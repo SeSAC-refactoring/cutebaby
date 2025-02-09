2b176f8ffc844a0321c0ac45f79dd902d2c18f1a
baejuhyang”git config --global user.email juhyang9897@gmail.com” <juhyang9897@gmail.com>
2025-02-10 00:39:07 +0900

    Header 선택된 메뉴는 스타일 바뀌도록 설정

--- src/components/commons/Header.tsx
+++ src/components/commons/Header.tsx
@@ -1,12 +1,14 @@
-import { Link } from "react-router-dom";
-import Logout from "../login-page/Logout";
-import styles from "../../styles/commons/Header.module.scss";
+import { Link, useLocation } from 'react-router-dom';
+import Logout from '../login-page/Logout';
+import styles from '../../styles/commons/Header.module.scss';
 // import logo from "/logo.png";
 
 export default function Header() {
-  return (
-    <>
-      {/* <div className={styles.header}>
+    const location = useLocation(); // 현재 URL 정보를 가져옴
+
+    return (
+        <>
+            {/* <div className={styles.header}>
                 <div>
                     <Link to="/Home">
                         <img src="/img/logo.png" alt="Logo" />
@@ -23,45 +25,57 @@ export default function Header() {
                 </div>
             </div> */}
 
-      <div className={styles.header}>
-        <div>
-          <Link to="/Home">
-            <img src="/img/logo.png" alt="Logo" />
-          </Link>
-        </div>
-        <div className={styles.menu_container}>
-          <div className={styles.menu}>
-            <Link
-              to="/Vaccination"
-              style={{ textDecoration: "none", color: "#3b3b41" }}
-            >
-              예방접종
-            </Link>
-          </div>
-          <div>
-            <Link to="/GrowthDiary" className={styles.menu_gray}>
-              성장기록
-            </Link>
-          </div>
-          <div>
-            <Link
-              to="/Mypage"
-              className={styles.menu_gray}
-              style={{ textDecoration: "none" }}
-            >
-              마이페이지
-            </Link>
-          </div>
-          {/* <button onClick={Logout}>로그아웃</button> */}
-          <div
-            className={styles.menu_gray}
-            style={{ cursor: "pointer" }}
-            onClick={Logout}
-          >
-            로그아웃
-          </div>
-        </div>
-      </div>
-    </>
-  );
+            <div className={styles.header}>
+                <div>
+                    <Link to="/Home">
+                        <img src="/img/logo.png" alt="Logo" />
+                    </Link>
+                </div>
+                <div className={styles.menu_container}>
+                    <Link
+                        to="/Vaccination"
+                        className={
+                            location.pathname === '/Vaccination'
+                                ? styles.menu
+                                : styles.menu_gray
+                        }
+                    >
+                        예방접종
+                    </Link>
+
+                    <Link
+                        to="/GrowthDiary"
+                        className={
+                            location.pathname === '/GrowthDiary'
+                                ? styles.menu
+                                : styles.menu_gray
+                        }
+                    >
+                        성장기록
+                    </Link>
+
+                    <div>
+                        <Link
+                            to="/Mypage"
+                            className={
+                                location.pathname === '/Mypage'
+                                    ? styles.menu
+                                    : styles.menu_gray
+                            }
+                        >
+                            마이페이지
+                        </Link>
+                    </div>
+                    {/* <button onClick={Logout}>로그아웃</button> */}
+                    <div
+                        className={styles.menu_gray}
+                        style={{ cursor: 'pointer' }}
+                        onClick={Logout}
+                    >
+                        로그아웃
+                    </div>
+                </div>
+            </div>
+        </>
+    );
 }
