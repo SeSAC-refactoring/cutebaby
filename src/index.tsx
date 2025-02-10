11ae5879fab0deb1bf87c2aed36f4cf056b2a5fb
baejuhyang”git config --global user.email juhyang9897@gmail.com” <juhyang9897@gmail.com>
2025-02-10 12:31:42 +0900

    로그인 css 완성

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import { Provider } from 'react-redux';
-import { store ,persistor} from './store';
+import { store, persistor } from './store';
 import { PersistGate } from 'redux-persist/integration/react';
-
+import './styles/commons/Reset.module.scss';
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
@@ -14,7 +14,7 @@ root.render(
     <BrowserRouter>
         <Provider store={store}>
             <PersistGate loading={null} persistor={persistor}>
-            <App />
+                <App />
             </PersistGate>
         </Provider>
     </BrowserRouter>
