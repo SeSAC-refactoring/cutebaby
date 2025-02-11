ab0a1587c8d335a1ff900393a63e422375871ae7
baejuhyang”git config --global user.email juhyang9897@gmail.com” <juhyang9897@gmail.com>
2025-02-12 02:20:00 +0900

    예방접종 페이지 오류 해결

--- src/pages/Vaccination.tsx
+++ src/pages/Vaccination.tsx
@@ -1,118 +1,134 @@
-import { Link } from "react-router-dom";
-import layout from "../styles/commons/Layout.module.scss";
-import typography from "../styles/commons/Typography.module.scss";
-import button from "../styles/commons/Button.module.scss";
-import styles from "../styles/Vaccination.module.scss";
-import { VaccinationTable } from "../components/vaccination-page/vaccination-table/VaccinationTable";
-import { useDispatch, useSelector } from "react-redux";
-import { AppDispatch, RootState } from "../store";
-import { useEffect, useState } from "react";
-import { fetchVaccinationData } from "../store/vaccinationSlice";
-import { BabyList } from "../components/commons/BabyList";
-import { useSelectBaby } from "../hooks/useSelectBaby";
-import { useVaccinationData } from "../components/vaccination-page/hooks/useVaccinationData";
-import { VaccineInfo } from "../components/vaccination-page/VaccineInfo";
-import { NeedLoginModal } from "../components/my-page/NeedLoginModal";
-import VaccinationCenters from "./VaccinationCenters";
-import VaccinationDetails from "./VaccinationDetails";
+import { Link } from 'react-router-dom';
+import layout from '../styles/commons/Layout.module.scss';
+import typography from '../styles/commons/Typography.module.scss';
+import button from '../styles/commons/Button.module.scss';
+import styles from '../styles/Vaccination.module.scss';
+import { VaccinationTable } from '../components/vaccination-page/vaccination-table/VaccinationTable';
+import { useDispatch, useSelector } from 'react-redux';
+import { AppDispatch, RootState } from '../store';
+import { useEffect, useState } from 'react';
+import { fetchVaccinationData } from '../store/vaccinationSlice';
+import { BabyList } from '../components/commons/BabyList';
+import { useSelectBaby } from '../hooks/useSelectBaby';
+import { useVaccinationData } from '../components/vaccination-page/hooks/useVaccinationData';
+import { VaccineInfo } from '../components/vaccination-page/VaccineInfo';
+import { NeedLoginModal } from '../components/my-page/NeedLoginModal';
+import VaccinationCenters from './VaccinationCenters';
+import VaccinationDetails from './VaccinationDetails';
 
 export default function Vaccination() {
-  const [openCentersModal, setOpenCentersModal] = useState<boolean>(false);
-  const [openDetailsModal, setOpenDetailsModal] = useState<boolean>(false);
+    const [openCentersModal, setOpenCentersModal] = useState<boolean>(false);
+    const [openDetailsModal, setOpenDetailsModal] = useState<boolean>(false);
 
-  const dispatch = useDispatch<AppDispatch>();
+    const dispatch = useDispatch<AppDispatch>();
 
-  const { babyInfo, nothingBaby } = useSelector(
-    (state: RootState) => state.baby
-  );
-  const { vaccinationData, loading, error } = useSelector(
-    (state: RootState) => state.vaccination
-  );
-  const { selectedBabyId, handleSelectBaby } = useSelectBaby(babyInfo);
-  const { selectedBabyVaccinationData } = useVaccinationData(
-    vaccinationData,
-    selectedBabyId
-  ); // selectedBabyVaccinationData = vaccinationData selectedBabyId에 따라 필터링 // selectedBabyId가 변경될 때 vaccinationData 업데이트
-  const [openLoginModal, setOpenLoginModal] = useState<boolean>(false);
+    const { babyInfo, nothingBaby } = useSelector(
+        (state: RootState) => state.baby
+    );
+    const { vaccinationData, loading, error } = useSelector(
+        (state: RootState) => state.vaccination
+    );
+    const { selectedBabyId, handleSelectBaby } = useSelectBaby(babyInfo);
+    const { selectedBabyVaccinationData } = useVaccinationData(
+        vaccinationData,
+        selectedBabyId
+    ); // selectedBabyVaccinationData = vaccinationData selectedBabyId에 따라 필터링 // selectedBabyId가 변경될 때 vaccinationData 업데이트
+    const [openLoginModal, setOpenLoginModal] = useState<boolean>(false);
 
-  const user = sessionStorage.getItem("user");
-  console.log("👼👼👼👼👼👼user", user);
+    const user = sessionStorage.getItem('user');
+    console.log('👼👼👼👼👼👼user', user);
 
-  // 로그인 안된 경우 로그인 모달 띄우기
-  useEffect(() => {
-    if (!user) {
-      setOpenLoginModal(true);
-    } else {
-      dispatch(fetchVaccinationData(5));
-      console.log("Updated vaccinationData:", vaccinationData);
-    }
-  }, [dispatch]);
+    // 로그인 안된 경우 로그인 모달 띄우기
+    useEffect(() => {
+        if (!user) {
+            setOpenLoginModal(true);
+        } else {
+            dispatch(fetchVaccinationData(5));
+            console.log('Updated vaccinationData:', vaccinationData);
+        }
+    }, [dispatch]);
 
-  return (
-    <>
-      <div className={layout.container}>
-        {/* {openLoginModal && (
+    return (
+        <>
+            <div className={layout.container}>
+                {/* {openLoginModal && (
               <NeedLoginModal
                   modalState={() => setOpenLoginModal(false)}
               />
           )} */}
-        <div className={layout.contentsArea}>
-          <div className={layout.titleArea}>
-            <div className={layout.textWrap}>
-              <div className={[layout.title, typography.text4xlBd].join(" ")}>
-                예방접종 관리
-              </div>
-              <div
-                className={[layout.pageGuide, typography.textXlMd].join(" ")}
-              >
-                <strong className={typography.textXlBd}>
-                  표준 예방접종 일정표
-                </strong>
-                를 기준으로 관리할 수 있어요:)
-              </div>
-            </div>
-            <div className={styles.button_wrap}>
-              <button
-                className={`${button.btnSmYw} ${typography.textBsBd}`}
-                onClick={() => setOpenCentersModal(true)}
-              >
-                위탁의료기관{" "}
-                <img src="img/icons/i-search-s20.svg" alt="성장일지 이미지" />
-                {/* <Link to="/VaccinationCenters">위탁의료기관 🔎</Link> */}
-                {/* {openCentersModal && <VaccinationCenters />} */}
-              </button>
-              <button
-                className={`${button.btnSmYw} ${typography.textBsBd}`}
-                onClick={() => setOpenDetailsModal(true)}
-              >
-                감염병 정보
-                {/* <Link to="/VaccinationDetails">감염병 정보</Link> */}
-              </button>
-              <button
-                className={`${button.btnSmYw} ${typography.textBsBd}`}
-                onClick={() => setOpenDetailsModal(true)}
-              >
-                국가예방접종
-              </button>
-            </div>
-          </div>
-          {openCentersModal && (
-            <VaccinationCenters setOpenCentersModal={setOpenCentersModal} />
-          )}
-          {openDetailsModal && (
-            <VaccinationDetails setOpenDetailsModal={setOpenDetailsModal} />
-          )}
+                <div className={layout.contentsArea}>
+                    <div className={layout.titleArea}>
+                        <div className={layout.textWrap}>
+                            <div
+                                className={[
+                                    layout.title,
+                                    typography.text4xlBd,
+                                ].join(' ')}
+                            >
+                                예방접종 관리
+                            </div>
+                            <div
+                                className={[
+                                    layout.pageGuide,
+                                    typography.textXlMd,
+                                ].join(' ')}
+                            >
+                                <strong className={typography.textXlBd}>
+                                    표준 예방접종 일정표
+                                </strong>
+                                를 기준으로 관리할 수 있어요:)
+                            </div>
+                        </div>
+                        <div className={styles.button_wrap}>
+                            <button
+                                className={`${button.btnSmYw} ${typography.textBsBd}`}
+                                onClick={() => setOpenCentersModal(true)}
+                            >
+                                위탁의료기관{' '}
+                                <img
+                                    src="img/icons/i-search-s20.svg"
+                                    alt="성장일지 이미지"
+                                />
+                                {/* <Link to="/VaccinationCenters">위탁의료기관 🔎</Link> */}
+                                {/* {openCentersModal && <VaccinationCenters />} */}
+                            </button>
+                            <button
+                                className={`${button.btnSmYw} ${typography.textBsBd}`}
+                                onClick={() => setOpenDetailsModal(true)}
+                            >
+                                감염병 정보
+                                {/* <Link to="/VaccinationDetails">감염병 정보</Link> */}
+                            </button>
+                            <button
+                                className={`${button.btnSmYw} ${typography.textBsBd}`}
+                                onClick={() => setOpenDetailsModal(true)}
+                            >
+                                국가예방접종
+                            </button>
+                        </div>
+                    </div>
+                    {openCentersModal && (
+                        <VaccinationCenters
+                            setOpenCentersModal={setOpenCentersModal}
+                        />
+                    )}
+                    {openDetailsModal && (
+                        <VaccinationDetails
+                            setOpenDetailsModal={setOpenDetailsModal}
+                        />
+                    )}
 
-          {openCentersModal && <VaccinationCenters />}
-          {openDetailsModal && <VaccinationDetails />}
-          {/* <VaccinationTable
-            selectedBabyVaccinationData={selectedBabyVaccinationData}
-          /> */}
-        </div>
+                    <VaccinationTable
+                        selectedBabyVaccinationData={
+                            selectedBabyVaccinationData
+                        }
+                        selectedBabyId={selectedBabyId}
+                    />
+                </div>
 
-        {/* 백신 정보 버튼 클릭 시 모달 예정 */}
-        <VaccineInfo />
-      </div>
-    </>
-  );
+                {/* 백신 정보 버튼 클릭 시 모달 예정 */}
+                <VaccineInfo />
+            </div>
+        </>
+    );
 }
