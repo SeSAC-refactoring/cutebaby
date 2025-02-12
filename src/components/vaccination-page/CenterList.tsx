fb26d840b8866fa9ac449d3d86705f7eb2e22ee6
baejuhyang”git config --global user.email juhyang9897@gmail.com” <juhyang9897@gmail.com>
2025-02-12 15:10:27 +0900

    의료기관 찾기 - 검색하기 전 검색해주세요 페이지 띄움

--- src/components/vaccination-page/CenterList.tsx
+++ src/components/vaccination-page/CenterList.tsx
@@ -1,87 +1,80 @@
-import React from "react";
-import { Centers } from "../types";
-import { VaccineList } from "./VaccineList";
-import styles from "../../styles/vaccination_unit.module.scss";
+import React from 'react';
+import { Centers } from '../types';
+import { VaccineList } from './VaccineList';
+import styles from '../../styles/vaccination_unit.module.scss';
 
 interface CenterListProps {
-  centers: Centers[];
-  toggleVaccineList: (orgcd: number) => void;
-  showVaccineList: { [key: number]: boolean };
+    centers: Centers[];
+    toggleVaccineList: (orgcd: number) => void;
+    showVaccineList: { [key: number]: boolean };
 }
 
 export const CenterList: React.FC<CenterListProps> = ({
-  centers,
-  toggleVaccineList,
-  showVaccineList,
+    centers,
+    toggleVaccineList,
+    showVaccineList,
 }) => {
-  return (
-    <ul>
-      {centers.map((center) => (
-        // <li key={center.orgcd}>
-        //   <strong>{center.orgnm}</strong>
-        //   <br />
-        //   {`주소: ${center.orgAddr}`}
-        //   <br />
-        //   {`전화번호: ${center.orgTlno}`}
-        //   <br />
-        //   <button onClick={() => toggleVaccineList(center.orgcd)}>
-        //     {showVaccineList[center.orgcd]
-        //       ? "접종 목록 숨기기▲"
-        //       : "접종 목록 보기▼"}
-        //   </button>
-
-        //   {/* 선택된 병원의 백신 목록 표시 */}
-        //   {showVaccineList[center.orgcd] && <VaccineList center={center} />}
-        // </li>
-        <div key={center.orgcd} className={styles.container}>
-          <div className={styles.content_wrap}>
-            <div
-              className={styles.contents_set}
-              style={{ justifyContent: "space-between" }}
-            >
-              <div className={`${styles.contents} ${styles.center_name}`}>
-                {center.orgnm}
-              </div>
-              <button
-                onClick={() => toggleVaccineList(center.orgcd)}
-                className={styles.list_btn}
-              >
-                {showVaccineList[center.orgcd] ? "접종 목록 ▲" : "접종 목록 ▼"}
-              </button>
-              {/* <div className={styles.vaccine_list}>
-                {showVaccineList[center.orgcd] && (
-                  <VaccineList center={center} />
-                )}
-              </div> */}
-            </div>
-            <div className={styles.contents_set}>
-              <div className={styles.contents_title}>병원주소</div>
-              <div className={styles.contents}>{center.orgAddr}</div>
-            </div>
-            <div className={styles.contents_set}>
-              <div className={styles.contents_title}>대표번호</div>
-              <img src="/img/phone-call-01.png" alt="전화기"></img>
-              <div className={`${styles.contents} ${styles.center_number}`}>
-                {center.orgTlno}
-              </div>
-            </div>
-            <div className={styles.btn_wrap}>
-              <div className={styles.vaccine_list}>
-                {showVaccineList[center.orgcd] && (
-                  <VaccineList center={center} />
-                )}
-              </div>
-              {/* <button
-                onClick={() => toggleVaccineList(center.orgcd)}
-                className={styles.list_btn}
-              >
-                {showVaccineList[center.orgcd] ? "접종 목록 ▲" : "접종 목록 ▼"}
-              </button> */}
-            </div>
-            {/* {showVaccineList[center.orgcd] && <VaccineList center={center} />} */}
-          </div>
+    return (
+        <div>
+            {centers && centers.length > 0 ? (
+                centers.map((center) => (
+                    <div key={center.orgcd} className={styles.container}>
+                        <div className={styles.content_wrap}>
+                            <div
+                                className={styles.contents_set}
+                                style={{ justifyContent: 'space-between' }}
+                            >
+                                <div
+                                    className={`${styles.contents} ${styles.center_name}`}
+                                >
+                                    {center.orgnm}
+                                </div>
+                                <button
+                                    onClick={() =>
+                                        toggleVaccineList(center.orgcd)
+                                    }
+                                    className={styles.list_btn}
+                                >
+                                    {showVaccineList[center.orgcd]
+                                        ? '접종 목록 ▲'
+                                        : '접종 목록 ▼'}
+                                </button>
+                            </div>
+                            <div className={styles.contents_set}>
+                                <div className={styles.contents_title}>
+                                    병원주소
+                                </div>
+                                <div className={styles.contents}>
+                                    {center.orgAddr}
+                                </div>
+                            </div>
+                            <div className={styles.contents_set}>
+                                <div className={styles.contents_title}>
+                                    대표번호
+                                </div>
+                                <img
+                                    src="/img/phone-call-01.png"
+                                    alt="전화기"
+                                ></img>
+                                <div
+                                    className={`${styles.contents} ${styles.center_number}`}
+                                >
+                                    {center.orgTlno}
+                                </div>
+                            </div>
+                            <div className={styles.btn_wrap}>
+                                <div className={styles.vaccine_list}>
+                                    {showVaccineList[center.orgcd] && (
+                                        <VaccineList center={center} />
+                                    )}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                ))
+            ) : (
+                <p>병원 정보가 없습니다</p>
+            )}
         </div>
-      ))}
-    </ul>
-  );
+    );
 };
--- src/components/vaccination-page/hooks/useHandleSearch.ts
+++ src/components/vaccination-page/hooks/useHandleSearch.ts
@@ -8,20 +8,19 @@ export const useHandleSearch = (
     searchCenters: (pageNo: number, inputAddress: string) => void,
     refs: RefsType
 ) => {
-    // 검색을 한 적이 있는지 추적 (useRef 사용) // 검색한 적 없다면 병원을 찾아보세요 띄워줌
-    const hasSearched = useRef(false);
-    // 드롭다운에 대한 ref 생성 // 입력하지 않았을 시 focus
-
+    const [hasSearched, setHasSearched] = useState<boolean>(false); // 검색을 한 적이 있는지 추적 // 검색한 적 없다면 병원을 찾아보세요 띄워줌
     const [inputAddress, setInputAddress] = useState<string>(''); // 주소 입력값 상태관리
     const [savedAddress, setSavedAddress] = useState<string>(''); // 검색 후 페이지 이동 시 저장된 주소 유지
 
     const handleSearch = useCallback(() => {
-        hasSearched.current = true; // 검색 수행 여부를 true로 변경
+        setHasSearched(true); // 검색 수행 여부 true
         if (!selectedLocation.province) {
+            setHasSearched(false);
             refs.province.current?.focus(); // 시/도를 선택하지 않았으면 focus
             return;
         }
         if (!selectedLocation.city) {
+            setHasSearched(false);
             refs.city.current?.focus(); // 시/군/구를 선택하지 않았으면 focus
             return;
         }
@@ -36,10 +35,10 @@ export const useHandleSearch = (
     ]);
 
     return {
-        hasSearched,
         inputAddress,
         setInputAddress,
         handleSearch,
         savedAddress,
+        hasSearched,
     };
 };
