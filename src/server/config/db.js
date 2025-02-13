c7d23a8fef99bca8fb3872f3e1f92fdfd3d9e82a
Yundo-git <ydh7804@gmail.com>
2025-02-14 02:10:46 +0900

    db설정변경

--- src/server/config/db.js
+++ src/server/config/db.js
@@ -9,8 +9,9 @@ const db = mysql.createConnection({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
-  port: process.env.DB_PORT,
+  port: parseInt(process.env.DB_PORT, 10),
 });
+
 
 db.connect((err) => {
   if (err) {
