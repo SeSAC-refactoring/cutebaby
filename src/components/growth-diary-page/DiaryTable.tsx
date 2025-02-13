5a0e4ca3924c004aecc84c5ac022d6c8086634cc
Yundo-git <ydh7804@gmail.com>
2025-02-13 16:13:58 +0900

    성장기록 수정/삭제 모달위치 수정

--- src/components/growth-diary-page/DiaryTable.tsx
+++ src/components/growth-diary-page/DiaryTable.tsx
@@ -6,40 +6,13 @@ import { GrowDelModal } from './GrowDelModal';
 
 interface DiaryTableProps {
     growData: newGrowData[];
+    onEdit: (growId: number) => void;
+    onDelete: (growId: number) => void;
 }
-export const DiaryTable: React.FC<DiaryTableProps> = ({ growData }) => {
-    const [rewriteModal, setRewriteModal] = useState<boolean>(false);
-    const [delModal , setdelModal] = useState<boolean>(false);
 
-    const [data, setData] = useState<newGrowData[]>(growData);
-    const [growId, setGrowId] = useState<number>(0);
-
-    console.log('data>>>', data);
-
-    const rewrite = (e: React.MouseEvent<HTMLButtonElement>) => {
-        setGrowId(Number(e.currentTarget.value));
-        setRewriteModal(true);
-    };
-
-    const delModalState = (e: React.MouseEvent<HTMLButtonElement>) => {
-        setGrowId(Number(e.currentTarget.value));
-        setdelModal(true);
-    };
+export const DiaryTable: React.FC<DiaryTableProps> = ({ growData, onEdit, onDelete }) => {
     return (
         <div className={styles.list_wrap}>
-            {rewriteModal && (
-                <GrowRewriteModal
-                    growId={growId}
-                    growData={data}
-                    onClose={() => setRewriteModal(false)}
-                />
-            )}
-            {delModal &&(
-                <GrowDelModal 
-                growId={growId}
-                growData={data}
-                onClose={() => setdelModal(false)}/>
-            )}
             <div className={styles.row_title}>
                 <div className={styles.list_title}>측정날짜</div>
                 <div className={styles.list_title}>키(cm)</div>
@@ -47,52 +20,27 @@ export const DiaryTable: React.FC<DiaryTableProps> = ({ growData }) => {
                 <div className={styles.list_title}>머리둘레(cm)</div>
             </div>
             <div>
-                {Array.isArray(growData) && growData.length > 0 ? (
+                {growData.length > 0 ? (
                     <ul>
                         {growData.map((info, i) => (
                             <li key={i} className={styles.row_el}>
-                                <div
-                                    className={styles.list_el}
-                                    style={{ paddingLeft: '0px' }}
-                                >
-                                    {info.inputData}
-                                </div>
-                                <div className={styles.list_el}>
-                                    {info.height}
-                                </div>
-                                <div className={styles.list_el}>
-                                    {info.weight}
-                                </div>
-                                <div className={styles.list_el}>
-                                    {info.head}
-                                </div>
-                                <button
-                                    className={`${styles.table_btn} ${styles.edit_btn}`}
-                                    value={info.id}
-                                    onClick={rewrite}
-                                >
+                                <div className={styles.list_el}>{info.inputData}</div>
+                                <div className={styles.list_el}>{info.height}</div>
+                                <div className={styles.list_el}>{info.weight}</div>
+                                <div className={styles.list_el}>{info.head}</div>
+                                <button className={`${styles.table_btn} ${styles.edit_btn}`}
+                                    onClick={() => onEdit(info.id)}>
                                     수정
-                                    <img
-                                        src="img/edit-02.png"
-                                        alt="수정 아이콘"
-                                        style={{ marginLeft: '4px' }}
-                                    ></img>
                                 </button>
-
-                                <button
-                                    className={`${styles.table_btn} ${styles.delete_btn}`}
-                                    value={info.id}
-                                    onClick={delModalState}
-                                >
+                                <button className={`${styles.table_btn} ${styles.delete_btn}`}
+                                    onClick={() => onDelete(info.id)}>
                                     삭제
                                 </button>
                             </li>
                         ))}
                     </ul>
                 ) : (
-                    <ul>
-                        <li className={styles.row_el}>성장 기록이 없습니다.</li>
-                    </ul>
+                    <ul><li className={styles.row_el}>성장 기록이 없습니다.</li></ul>
                 )}
             </div>
         </div>
