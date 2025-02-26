1672fff9417acd3f24e00b0a07b6acf52b29ddd8
baejuhyang”git config --global user.email juhyang9897@gmail.com” <juhyang9897@gmail.com>
2025-02-26 18:01:30 +0900

    breakpoint 390px xs로 생성

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
     content: ['./src/**/*.{js,jsx,ts,tsx}', './index.tsx'], // 프로젝트 전체의 모든 JSX, TSX 파일에서 Tailwind 사용 가능
     theme: {
-        extend: {},
+        extend: {
+            screens: {
+                xs: '390px', // 390px 이상일 때 적용
+            },
+        },
     },
     plugins: [],
 };
