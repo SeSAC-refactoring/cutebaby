dccaed1bfa770a18ed551be8444c43406367284d
yujin08 <kkkyu28@naver.com>
2025-03-10 01:41:56 +0900

    마이페이지 모달 수정

--- src/components/commons/GenderInputComponent.tsx
+++ src/components/commons/GenderInputComponent.tsx
@@ -16,34 +16,23 @@ const GenderInputComponent: React.FC<GenderInputProps> = ({
 
   return (
     <div
-      style={{ display: "flex", gap: "20px" }}
-      className="flex gap-5 bg-blue-1 border-2 border-blue-5 rounded-[16px] mb-[49px]"
+      className="flex gap-5 bg-blue-1 border-2 border-blue-5 rounded-[20px] mb-[49px]"
       onClick={(e) => e.stopPropagation()}
     >
       <div className="w-full flex">
         {genderOptions.map((el) => (
           <button
             key={el.value}
-            // className={
-            //   el.value === selectedGender
-            //     ? styles.segmentsItem_selected
-            //     : styles.segmentsItem
-            // }
             onClick={(e) => {
               e.stopPropagation();
               if (selectedGender !== el.value) {
                 setSelectedGender(el.value);
               }
             }}
-            style={{
-              cursor: "pointer",
-              fontWeight: el.value === selectedGender ? "bold" : "normal",
-            }}
-            // className="rounded-[16px] p-4 gap-2 flex-[1_0_0] justify-around"
             className={`${
               el.value === selectedGender
-                ? "rounded-[16px] p-4 gap-2 flex-[1_0_0] justify-around bg-blue-3 border-2 border-blue-7 text-sm font-bd text-blue-9"
-                : "rounded-[16px] p-4 gap-2 flex-[1_0_0] justify-around text-sm font-md text-blue-8"
+                ? "rounded-[16px] p-4 gap-2 flex-[1_0_0] justify-around bg-blue-3 border-2 border-blue-7 text-sm font-bd text-blue-9 cursor-pointer"
+                : "rounded-[16px] p-4 gap-2 flex-[1_0_0] justify-around text-sm font-md text-blue-8 cursor-pointer"
             }`}
           >
             {el.text}
