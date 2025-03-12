faf94d4ecf4fbbee004dfc74606938abd33e02e6
Yundo-git <ydh7804@gmail.com>
2025-03-12 19:46:10 +0900

    예방접종 테이블 진행중

--- src/components/vaccination-page/vaccination-table/VaccinationTable.tsx
+++ src/components/vaccination-page/vaccination-table/VaccinationTable.tsx
@@ -92,68 +92,77 @@ export const VaccinationTable: React.FC<VaccinationTableProps> = ({
       </section>
 
       <section>
-        <table className="w-full border-collapse border border-gray-300">
-          <thead>
+        <table className="w-full mt-6  border-separate border-spacing-0">
+          <thead className=" bg-blue-3 text-blue-8 ">
             <tr>
-              <th className="border border-gray-300 p-2">대상 감염병</th>
-              <th className="border border-gray-300 p-2">백신 종류</th>
-              <th className="border border-gray-300 p-2">권장 접종 횟수</th>
-              <th className="border border-gray-300 p-2">접종 횟수</th>
-              <th className="border border-gray-300 p-2">관리</th>
+              <th className="text-left p-4 rounded-l-[0.5rem]">대상 감염병</th>
+              <th className="text-left p-4">백신 종류</th>
+              <th className="p-4">권장횟수</th>
+              <th>완료횟수</th>
+              <th className="p-4 rounded-r-[0.5rem]">관리</th>
             </tr>
+            {/* 테이블 사이 넓히는 용도 */}
+            <tr className="after:content-[''] after:block after:h-6 bg-white"></tr>
           </thead>
           <tbody>
-            {diseasesResult.map((diseaseIndex) => {
-              const disease = diseasesData[diseaseIndex];
+            {diseasesResult.reduce<React.ReactElement[]>(
+              (acc, diseaseIndex, diseaseIdx) => {
+                const disease = diseasesData[diseaseIndex];
 
-              const matchedVaccines = Array.isArray(disease.vaccinationid)
-                ? vaccinesData.filter((v) =>
-                    (disease.vaccinationid as number[]).includes(
-                      Number(v.vaccinationid)
+                const matchedVaccines = Array.isArray(disease.vaccinationid)
+                  ? vaccinesData.filter((v) =>
+                      (disease.vaccinationid as number[]).includes(
+                        Number(v.vaccinationid)
+                      )
                     )
-                  )
-                : vaccinesData.filter(
-                    (v) => v.vaccinationid === disease.vaccinationid
-                  );
+                  : vaccinesData.filter(
+                      (v) => v.vaccinationid === disease.vaccinationid
+                    );
 
-              return matchedVaccines.length > 0 ? (
-                matchedVaccines.map((vaccine, index) => (
-                  <tr key={`${diseaseIndex}-${vaccine.vaccinationid}`}>
-                    {/* 첫 번째 백신일 경우만 감염병명 td 출력 */}
-                    {index === 0 && (
-                      <td
-                        rowSpan={matchedVaccines.length}
-                        className="border border-gray-300 p-2"
-                      >
-                        {disease.name}
+                const isEvenDisease = diseaseIdx % 2 === 0;
+                //색깔바꾸기
+                const rowColor = isEvenDisease ? "bg-blue-1" : "bg-white";
+
+                matchedVaccines.forEach((vaccine, index) => {
+                  acc.push(
+                    <tr
+                      key={`${diseaseIndex}-${vaccine.vaccinationid}`}
+                      className={rowColor}
+                    >
+                      {index === 0 ? (
+                        <td className="p-4" rowSpan={matchedVaccines.length}>
+                          {disease.name}
+                        </td>
+                      ) : null}
+                      {/* 백신이름 */}
+                      <td className="w-[50%] p-4">{vaccine.name}</td>
+                      {/* 권장횟수 */}
+                      <td className="w-[10%] text-center p-4">
+                        {vaccine.doses}
                       </td>
-                    )}
-                    {/* 백신 종류 */}
-                    <td className="border border-gray-300 p-2">
-                      {vaccine.name}
-                    </td>
-                    {/* 권장 접종횟수 */}
-                    <td className="border border-gray-300 p-2">
-                      {vaccine.doses}
-                    </td>
-                    <td>
-                      <VaccineType
-                        selectedBabyId={selectedBabyId}
-                        vaccineIds={
-                          Array.isArray(disease.vaccinationid)
-                            ? disease.vaccinationid
-                            : [disease.vaccinationid]
-                        }
-                      />
-                    </td>
-                  </tr>
-                ))
-              ) : (
-                <tr key={diseaseIndex}>
-                  <td className="border border-gray-300 p-2">{disease.name}</td>
-                </tr>
-              );
-            })}
+                      {/* 완료횟수 */}
+                      <td className="w-[10%] text-center p-4">1</td>
+                      {/* 관리버튼 */}
+                      <td className="p-2">
+                        <div className="flex justify-center p-4items-center h-full">
+                          <VaccineType
+                            selectedBabyId={selectedBabyId}
+                            vaccineIds={
+                              Array.isArray(disease.vaccinationid)
+                                ? disease.vaccinationid
+                                : [disease.vaccinationid]
+                            }
+                          />
+                        </div>
+                      </td>
+                    </tr>
+                  );
+                });
+
+                return acc;
+              },
+              [] // 빈 배열을 초기값으로 설정
+            )}
           </tbody>
         </table>
       </section>
