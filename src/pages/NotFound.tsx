ac7d0c898d4f0d4d436c5c8b0e3eb22e17a4c617
yujin08 <kkkyu28@naver.com>
2025-03-20 17:39:59 +0900

    404페이지 수정

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -4,17 +4,25 @@ export const NotFound = () => {
   const navigate = useNavigate();
 
   return (
-    <main>
-      <img src="/img/404.png" alt="test" />
-
-      <button onClick={() => navigate(-1)} className="button-lg button-yellow">
-        이전페이지로 바로가기
-        <img src="../img/icons/i-chevron-right-s20.svg" alt="" />
-      </button>
-      <Link to="/Home" className="button-lg button-black">
-        홈으로 바로가기
-        <img src="../img/icons/i-chevron-right-s20.svg" alt="" />
-      </Link>
+    <main className="w-full">
+      <div className="flex flex-col gap-4 items-center justify-center xl:w-1/2 w-full p-5">
+        <img src="/img/404.png" alt="test" />
+        <div className="flex gap-2 justify-center">
+          <button
+            onClick={() => navigate(-1)}
+            className="button-sm button-yellow"
+          >
+            이전페이지로 바로가기
+            <img src="../img/icons/i-chevron-right-s20.svg" alt="" />
+          </button>
+          <Link to="/">
+            <button className="button-sm button-black">
+              홈으로 바로가기
+              <img src="../img/icons/i-chevron-right-s20.svg" alt="" />
+            </button>
+          </Link>
+        </div>
+      </div>
     </main>
   );
 };
