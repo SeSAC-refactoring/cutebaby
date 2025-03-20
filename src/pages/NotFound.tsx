fea4a7044d4a1d5f3e6fa2bd8f7c914e6b8f04e1
baejuhyang”git config --global user.email juhyang9897@gmail.com” <juhyang9897@gmail.com>
2025-03-20 20:32:47 +0900

    404페이지 width 390px 미만일 때 버튼 넘치는 것 수정

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,28 +1,34 @@
-import { Link, useNavigate } from "react-router-dom";
+import { Link, useNavigate } from 'react-router-dom';
 
 export const NotFound = () => {
-  const navigate = useNavigate();
+    const navigate = useNavigate();
 
-  return (
-    <main className="w-full">
-      <div className="flex flex-col gap-4 items-center justify-center xl:w-1/2 w-full p-5">
-        <img src="/img/404.png" alt="test" />
-        <div className="flex gap-2 justify-center">
-          <button
-            onClick={() => navigate(-1)}
-            className="button-sm button-yellow"
-          >
-            이전페이지로 바로가기
-            <img src="../img/icons/i-chevron-right-s20.svg" alt="" />
-          </button>
-          <Link to="/">
-            <button className="button-sm button-black">
-              홈으로 바로가기
-              <img src="../img/icons/i-chevron-right-s20.svg" alt="" />
-            </button>
-          </Link>
-        </div>
-      </div>
-    </main>
-  );
+    return (
+        <main className="w-full">
+            <div className="flex flex-col gap-4 items-center sm:justify-center xl:w-1/2 w-full p-5">
+                <img src="/img/404.png" alt="test" />
+                <div className="flex flex-col w-full ssm:flex-row gap-2 justify-center items-center">
+                    <button
+                        onClick={() => navigate(-1)}
+                        className="button-sm button-yellow w-full"
+                    >
+                        이전페이지로 바로가기
+                        <img
+                            src="../img/icons/i-chevron-right-s20.svg"
+                            alt=""
+                        />
+                    </button>
+                    <Link to="/" className="w-full">
+                        <button className="button-sm button-black w-full">
+                            홈으로 바로가기
+                            <img
+                                src="../img/icons/i-chevron-right-s28.svg"
+                                alt=""
+                            />
+                        </button>
+                    </Link>
+                </div>
+            </div>
+        </main>
+    );
 };
