297943a1c0647e79c9f87289cc7a4854b6ee2475
JH <juhyang9897@gmail.com>
2025-03-23 13:09:05 +0900

    마이페이지 등록된아이가 없습니다 페이지 수정

--- src/components/my-page/UserupdateModal.tsx
+++ src/components/my-page/UserupdateModal.tsx
@@ -1,237 +1,256 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
-import { Input, InputSignupEmail } from "../commons/Input";
+import { Input, InputSignupEmail } from '../commons/Input';
 
 interface UserupdateProps {
-  modalState: () => void;
+    modalState: () => void;
 }
 
 export const UserupdateModal: React.FC<UserupdateProps> = ({ modalState }) => {
-  const API_URL = process.env.REACT_APP_API_URL;
+    const API_URL = process.env.REACT_APP_API_URL;
 
-  const [formData, setFormData] = useState({
-    email: sessionStorage.getItem("useremail") || "",
-    name: sessionStorage.getItem("username") || "",
-  });
-  const [messages, setMessages] = useState({
-    email: "",
-    name: "",
-  });
+    const [formData, setFormData] = useState({
+        email: sessionStorage.getItem('useremail') || '',
+        name: sessionStorage.getItem('username') || '',
+    });
+    const [messages, setMessages] = useState({
+        email: '',
+        name: '',
+    });
 
-  const [emailbtn, setEmailCheck] = useState<boolean>(false);
+    const [emailbtn, setEmailCheck] = useState<boolean>(false);
 
-  // 입력 필드 변경 핸들러
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = e.target;
-    setFormData((prev) => ({
-      ...prev,
-      [name]: value,
-    }));
-  };
-
-  // 이메일 유효성 검사
-  useEffect(() => {
-    if (!formData.email.trim()) {
-      setMessages((prev) => ({ ...prev, email: "" }));
-      return;
-    }
-
-    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-
-    if (emailbtn) {
-      setMessages((prev) => ({
-        ...prev,
-        email: emailRegex.test(formData.email)
-          ? "사용 가능한 이메일입니다!"
-          : "이메일 형식이 올바르지 않습니다!",
-      }));
-    } else {
-      setMessages((prev) => ({
-        ...prev,
-        email: emailRegex.test(formData.email)
-          ? "중복체크를 눌러주세요!"
-          : "이메일 형식이 올바르지 않습니다!",
-      }));
-      setEmailCheck(false);
-    }
-  }, [formData.email]);
-
-  // 이메일 중복 확인 요청
-  const emailCheck = async (e: React.FormEvent) => {
-    e.preventDefault();
-    // console.log("formdata.email>>>", formData.email);
-    // console.log("formdata.email type >>", typeof formData.email);
-
-    if (!formData.email.trim()) {
-      setMessages((prev) => ({ ...prev, email: "이메일을 입력해주세요!" }));
-      return;
-    }
-    if (!formData.email.includes("@")) {
-      setMessages((prev) => ({
-        ...prev,
-        email: "유효하지 않은 이메일 주소입니다!",
-      }));
-      return;
-    }
-    try {
-      const response = await axios.post(`${API_URL}/emailCheck`, {
-        inputEmail: formData.email,
-      });
-
-      // console.log("서버 응답:", response); // 디버깅 로그 추가
-      // console.log("서버에서 응답받은 user", response.data.user[0].userid);
-      if (response.data.success) {
-        setEmailCheck(true);
-        setMessages((prev) => ({
-          ...prev,
-          email: "사용 가능한 이메일입니다!",
+    // 입력 필드 변경 핸들러
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = e.target;
+        setFormData((prev) => ({
+            ...prev,
+            [name]: value,
         }));
-      } else {
-        setEmailCheck(false);
-        setMessages((prev) => ({
-          ...prev,
-          email: "이미 사용 중인 이메일입니다!",
-        }));
-      }
-    } catch (error) {
-      // console.error(" 이메일 중복 확인 오류:", error);
-      setMessages((prev) => ({
-        ...prev,
-        email: "이메일 중복 확인 중 오류가 발생했습니다.",
-      }));
-    }
-  };
+    };
 
-  // 사용자 정보 업데이트 요청
-  const handleUpdate = async () => {
-    const storedName = sessionStorage.getItem("username") || "";
-    const storedEmail = sessionStorage.getItem("useremail") || "";
+    // 이메일 유효성 검사
+    useEffect(() => {
+        if (!formData.email.trim()) {
+            setMessages((prev) => ({ ...prev, email: '' }));
+            return;
+        }
 
-    const updatedName = formData.name.trim() || storedName;
-    const updatedEmail = formData.email.trim() || storedEmail;
+        const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
-    if (updatedName === storedName && updatedEmail === storedEmail) {
-      modalState();
-      return;
-    }
+        if (emailbtn) {
+            setMessages((prev) => ({
+                ...prev,
+                email: emailRegex.test(formData.email)
+                    ? '사용 가능한 이메일입니다!'
+                    : '이메일 형식이 올바르지 않습니다!',
+            }));
+        } else {
+            setMessages((prev) => ({
+                ...prev,
+                email: emailRegex.test(formData.email)
+                    ? '중복체크를 눌러주세요!'
+                    : '이메일 형식이 올바르지 않습니다!',
+            }));
+            setEmailCheck(false);
+        }
+    }, [formData.email]);
 
-    if (!updatedName) {
-      setMessages((prev) => ({ ...prev, name: "이름을 입력해주세요!" }));
-      return;
-    }
-    if (!updatedEmail) {
-      setMessages((prev) => ({ ...prev, email: "이메일을 입력해주세요!" }));
-      return;
-    }
-    if (!emailbtn && updatedEmail !== storedEmail) {
-      setMessages((prev) => ({
-        ...prev,
-        email: "이메일 중복 체크를 해주세요!",
-      }));
-      return;
-    }
+    // 이메일 중복 확인 요청
+    const emailCheck = async (e: React.FormEvent) => {
+        e.preventDefault();
+        // console.log("formdata.email>>>", formData.email);
+        // console.log("formdata.email type >>", typeof formData.email);
 
-    const usernum = sessionStorage.getItem("usernumber");
+        if (!formData.email.trim()) {
+            setMessages((prev) => ({
+                ...prev,
+                email: '이메일을 입력해주세요!',
+            }));
+            return;
+        }
+        if (!formData.email.includes('@')) {
+            setMessages((prev) => ({
+                ...prev,
+                email: '유효하지 않은 이메일 주소입니다!',
+            }));
+            return;
+        }
+        try {
+            const response = await axios.post(`${API_URL}/emailCheck`, {
+                inputEmail: formData.email,
+            });
 
-    try {
-      const response = await axios.post(`${API_URL}/updateUser`, {
-        email: updatedEmail,
-        username: updatedName,
-        usernumber: usernum,
-      });
+            // console.log("서버 응답:", response); // 디버깅 로그 추가
+            // console.log("서버에서 응답받은 user", response.data.user[0].userid);
+            if (response.data.success) {
+                setEmailCheck(true);
+                setMessages((prev) => ({
+                    ...prev,
+                    email: '사용 가능한 이메일입니다!',
+                }));
+            } else {
+                setEmailCheck(false);
+                setMessages((prev) => ({
+                    ...prev,
+                    email: '이미 사용 중인 이메일입니다!',
+                }));
+            }
+        } catch (error) {
+            // console.error(" 이메일 중복 확인 오류:", error);
+            setMessages((prev) => ({
+                ...prev,
+                email: '이메일 중복 확인 중 오류가 발생했습니다.',
+            }));
+        }
+    };
 
-      if (response.data.success) {
-        alert("정보가 성공적으로 수정되었습니다.");
+    // 사용자 정보 업데이트 요청
+    const handleUpdate = async () => {
+        const storedName = sessionStorage.getItem('username') || '';
+        const storedEmail = sessionStorage.getItem('useremail') || '';
 
-        // sessionStorage 업데이트
-        sessionStorage.setItem("username", updatedName);
-        sessionStorage.setItem("useremail", updatedEmail);
+        const updatedName = formData.name.trim() || storedName;
+        const updatedEmail = formData.email.trim() || storedEmail;
 
-        // 스토리지 이벤트 트리거 (Mypage에서 리렌더링)
-        window.dispatchEvent(new Event("storage"));
+        if (updatedName === storedName && updatedEmail === storedEmail) {
+            modalState();
+            return;
+        }
 
-        // 모달 닫기
-        modalState();
-      } else {
-        alert("정보 수정에 실패했습니다.");
-      }
-    } catch (error) {
-      // console.error("정보 수정 오류:", error);
-    }
-  };
-  return (
-    <div onClick={modalState} className="modalBg">
-      <div onClick={(e) => e.stopPropagation()} className="mediumModal">
-        <div className="flex justify-between">
-          <div className="text-2xl font-bd sm:mb-10">개인 정보 수정</div>
-          <div
-            onClick={() => {
-              modalState();
-            }}
-          >
-            <img src="img/Button-close.png" alt="" />
-          </div>
-        </div>
-        <div className="flex flex-col h-full w-full">
-          <div className="gap-2 mb-6 flex flex-col">
-            {/* 이름 입력 */}
-            <Input
-              label="이름"
-              name="name"
-              placeholder="이름 입력"
-              value={formData.name}
-              onChange={handleChange}
-            ></Input>
-            <p>{messages.name}</p>
+        if (!updatedName) {
+            setMessages((prev) => ({ ...prev, name: '이름을 입력해주세요!' }));
+            return;
+        }
+        if (!updatedEmail) {
+            setMessages((prev) => ({
+                ...prev,
+                email: '이메일을 입력해주세요!',
+            }));
+            return;
+        }
+        if (!emailbtn && updatedEmail !== storedEmail) {
+            setMessages((prev) => ({
+                ...prev,
+                email: '이메일 중복 체크를 해주세요!',
+            }));
+            return;
+        }
 
-            {/* 이메일 입력 */}
-            <div className="flex flex-col gap-2">
-              <div className="flex items-end gap-4">
-                <Input
-                  label="이메일"
-                  type="email"
-                  name="email"
-                  placeholder="이메일 입력"
-                  value={formData.email}
-                  onChange={handleChange}
-                />
-                <button className="button-md button-blue" onClick={emailCheck}>
-                  중복 체크
-                </button>
-              </div>
-              {messages.email ? (
-                messages.email !== "사용 가능한 이메일입니다!" ? (
-                  <p className="text-red-6">{messages.email}</p>
-                ) : (
-                  <p className="text-gray-8">{messages.email}</p>
-                )
-              ) : (
-                <p className="text-red-6">중복체크를 눌러주세요!</p>
-              )}
+        const usernum = sessionStorage.getItem('usernumber');
+
+        try {
+            const response = await axios.post(`${API_URL}/updateUser`, {
+                email: updatedEmail,
+                username: updatedName,
+                usernumber: usernum,
+            });
+
+            if (response.data.success) {
+                alert('정보가 성공적으로 수정되었습니다.');
+
+                // sessionStorage 업데이트
+                sessionStorage.setItem('username', updatedName);
+                sessionStorage.setItem('useremail', updatedEmail);
+
+                // 스토리지 이벤트 트리거 (Mypage에서 리렌더링)
+                window.dispatchEvent(new Event('storage'));
+
+                // 모달 닫기
+                modalState();
+            } else {
+                alert('정보 수정에 실패했습니다.');
+            }
+        } catch (error) {
+            // console.error("정보 수정 오류:", error);
+        }
+    };
+    return (
+        <div onClick={modalState} className="modalBg">
+            <div onClick={(e) => e.stopPropagation()} className="mediumModal">
+                <div className="flex justify-between">
+                    <div className="text-2xl font-bd sm:mb-10">
+                        개인 정보 수정
+                    </div>
+                    <div
+                        onClick={() => {
+                            modalState();
+                        }}
+                    >
+                        <img
+                            className="close"
+                            src="img/Button-close.png"
+                            alt="닫기"
+                        />
+                    </div>
+                </div>
+                <div className="flex flex-col h-full w-full">
+                    <div className="gap-2 mb-6 flex flex-col">
+                        {/* 이름 입력 */}
+                        <Input
+                            label="이름"
+                            name="name"
+                            placeholder="이름 입력"
+                            value={formData.name}
+                            onChange={handleChange}
+                        ></Input>
+                        <p>{messages.name}</p>
+
+                        {/* 이메일 입력 */}
+                        <div className="flex flex-col gap-2">
+                            <div className="flex items-end gap-4">
+                                <Input
+                                    label="이메일"
+                                    type="email"
+                                    name="email"
+                                    placeholder="이메일 입력"
+                                    value={formData.email}
+                                    onChange={handleChange}
+                                />
+                                <button
+                                    className="button-md button-blue"
+                                    onClick={emailCheck}
+                                >
+                                    중복 체크
+                                </button>
+                            </div>
+                            {messages.email ? (
+                                messages.email !==
+                                '사용 가능한 이메일입니다!' ? (
+                                    <p className="text-red-6">
+                                        {messages.email}
+                                    </p>
+                                ) : (
+                                    <p className="text-gray-8">
+                                        {messages.email}
+                                    </p>
+                                )
+                            ) : (
+                                <p className="text-red-6">
+                                    중복체크를 눌러주세요!
+                                </p>
+                            )}
+                        </div>
+                    </div>
+                    <section className="flex flex-col h-auto ">
+                        <div className="flex justify-center gap-4">
+                            <button
+                                onClick={modalState}
+                                className="button button-yellow button-lg w-full"
+                            >
+                                취소
+                            </button>
+                            <button
+                                className="button-black button-lg w-full"
+                                onClick={handleUpdate}
+                            >
+                                완료
+                            </button>
+                        </div>
+                    </section>
+                </div>
             </div>
-            {/* </label> */}
-
-            {/* 이메일 중복 확인 버튼 */}
-          </div>
-          <section className="flex flex-col h-auto ">
-            <div className="flex justify-center gap-4">
-              <button
-                onClick={modalState}
-                className="button button-yellow button-lg w-full"
-              >
-                취소
-              </button>
-              <button
-                className="button-black button-lg w-full"
-                onClick={handleUpdate}
-              >
-                완료
-              </button>{" "}
-            </div>
-          </section>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
--- src/pages/Mypage.tsx
+++ src/pages/Mypage.tsx
@@ -1,144 +1,148 @@
-import { useEffect, useState } from "react";
-import { useSelector } from "react-redux";
-import { RootState } from "../store";
-import { UserupdateModal } from "../components/my-page/UserupdateModal";
-import { BabyInfo } from "../components/my-page/BabyInfo";
-import { useNavigate } from "react-router-dom";
-import { BabyInputPlus } from "../components/my-page/BabyInputPlus";
+import { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import { RootState } from '../store';
+import { UserupdateModal } from '../components/my-page/UserupdateModal';
+import { BabyInfo } from '../components/my-page/BabyInfo';
+import { useNavigate } from 'react-router-dom';
+import { BabyInputPlus } from '../components/my-page/BabyInputPlus';
 
 export default function Mypage() {
-  // const dispatch = useDispatch<AppDispatch>();
+    // const dispatch = useDispatch<AppDispatch>();
 
-  // 사용자 정보를 useState로 관리 (실시간 반영)
-  const storedEmail = sessionStorage.getItem("useremail") ?? "";
-  const isKakaoLogin = !storedEmail.includes("@"); // 이메일에 '@'있는지 확인하기 없으면 카톡로그인
-  const [userInfo, setUserInfo] = useState({
-    username: sessionStorage.getItem("username") ?? "방문자님",
-    userid: isKakaoLogin ? "카카오 로그인" : storedEmail, // 카톡로그인이면 카카오로그인 출력 아님 세션에 저장된거 출력
-  });
+    // 사용자 정보를 useState로 관리 (실시간 반영)
+    const storedEmail = sessionStorage.getItem('useremail') ?? '';
+    const isKakaoLogin = !storedEmail.includes('@'); // 이메일에 '@'있는지 확인하기 없으면 카톡로그인
+    const [userInfo, setUserInfo] = useState({
+        username: sessionStorage.getItem('username') ?? '방문자님',
+        userid: isKakaoLogin ? '카카오 로그인' : storedEmail, // 카톡로그인이면 카카오로그인 출력 아님 세션에 저장된거 출력
+    });
 
-  // const [openModal, setOpenModal] = useState<boolean>(false);
-  const [updateModal, setOpenUpdate] = useState<boolean>(false);
-  const [babyPlus, setBabyPlus] = useState<boolean>(false);
-  const { babyInfo, nothingBaby } = useSelector(
-    (state: RootState) => state.baby
-  );
+    // const [openModal, setOpenModal] = useState<boolean>(false);
+    const [updateModal, setOpenUpdate] = useState<boolean>(false);
+    const [babyPlus, setBabyPlus] = useState<boolean>(false);
+    const { babyInfo, nothingBaby } = useSelector(
+        (state: RootState) => state.baby
+    );
 
-  //  로그인 안했을 시 // 로그인 페이지로 리디렉션
-  const user = sessionStorage.getItem("user");
-  const navigate = useNavigate();
-  useEffect(() => {
-    if (!user) {
-      navigate("/");
-    }
-  });
+    //  로그인 안했을 시 // 로그인 페이지로 리디렉션
+    const user = sessionStorage.getItem('user');
+    const navigate = useNavigate();
+    useEffect(() => {
+        if (!user) {
+            navigate('/');
+        }
+    });
 
-  const update = () => {
-    if (!isKakaoLogin) {
-      setOpenUpdate(true);
-    }
-  };
-
-  // sessionStorage가 변경되면 state 업데이트
-  useEffect(() => {
-    const handleStorageChange = () => {
-      const updatedEmail = sessionStorage.getItem("useremail") ?? "";
-      const kakaoLogin = !updatedEmail.includes("@");
-
-      setUserInfo({
-        username: sessionStorage.getItem("username") ?? "방문자님",
-        userid: kakaoLogin ? "카카오 로그인" : updatedEmail,
-      });
+    const update = () => {
+        if (!isKakaoLogin) {
+            setOpenUpdate(true);
+        }
     };
 
-    window.addEventListener("storage", handleStorageChange);
-    return () => {
-      window.removeEventListener("storage", handleStorageChange);
-    };
-  }, []);
-
-  return (
-    <main>
-      {babyPlus && (
-        <div
-          onClick={() => setBabyPlus(false)}
-          // style={{ right: '0px', top: '70px' }}
-        >
-          <BabyInputPlus
-            onClose={() => setBabyPlus(false)}
-            babyInfo={babyInfo}
-          />
-        </div>
-      )}
-
-      {/* 수정 모달 */}
-      {updateModal && (
-        <UserupdateModal
-          modalState={() => {
-            setOpenUpdate(false);
-            // 정보가 수정되면 상태 업데이트
-            const updatedEmail = sessionStorage.getItem("useremail") ?? "";
-            const kakaoLogin = !updatedEmail.includes("@");
+    // sessionStorage가 변경되면 state 업데이트
+    useEffect(() => {
+        const handleStorageChange = () => {
+            const updatedEmail = sessionStorage.getItem('useremail') ?? '';
+            const kakaoLogin = !updatedEmail.includes('@');
 
             setUserInfo({
-              username: sessionStorage.getItem("username") ?? "방문자님",
-              userid: kakaoLogin ? "카카오 로그인" : updatedEmail,
+                username: sessionStorage.getItem('username') ?? '방문자님',
+                userid: kakaoLogin ? '카카오 로그인' : updatedEmail,
             });
-          }}
-        />
-      )}
+        };
 
-      <div className="whitebox">
-        <h2>마이페이지</h2>
-        <div className="mypageContentsArea">
-          {/* 사용자 정보 */}
-          <div className="userArea">
-            <div className="topArea">
-              <h3>내 정보</h3>
-              <button onClick={update} disabled={isKakaoLogin}>
-                개인정보 수정
-                <img src="/img/edit-01.png" alt="수정 아이콘" />
-              </button>
-            </div>
+        window.addEventListener('storage', handleStorageChange);
+        return () => {
+            window.removeEventListener('storage', handleStorageChange);
+        };
+    }, []);
 
-            <div className="userInfo">
-              <div className="item">
-                <div className="item-label">이름</div>
-                <div className="item-value">{userInfo.username}</div>
-              </div>
-              <div className="item">
-                <div className="item-label">이메일</div>
-                <div className="item-value">{userInfo.userid}</div>
-              </div>
-            </div>
-          </div>
-
-          {/* 애기 정보 */}
-          {babyInfo.length > 0 ? (
-            <BabyInfo babyInfo={babyInfo} />
-          ) : (
-            <div className="w-full h-[34.75rem]  flex justify-center items-center  ">
-              <div className="flex flex-col items-center  gap-6 ">
-                <img src="/img/Ggoggo-002.png" />
-                <div className="text-xl font-md text-gray-6 ">
-                  등록된 아이가 없습니다!
+    return (
+        <main>
+            {babyPlus && (
+                <div onClick={() => setBabyPlus(false)}>
+                    <BabyInputPlus
+                        onClose={() => setBabyPlus(false)}
+                        babyInfo={babyInfo}
+                    />
                 </div>
+            )}
 
-                <button
-                  onClick={() => setBabyPlus(true)}
-                  className="button button-black button-lg"
-                >
-                  아이 등록하기
-                  <img
-                    src="../img/icons/i-chevron-right-s28.svg"
-                    alt="바로가기 아이콘"
-                  />
-                </button>
-              </div>
+            {/* 수정 모달 */}
+            {updateModal && (
+                <UserupdateModal
+                    modalState={() => {
+                        setOpenUpdate(false);
+                        // 정보가 수정되면 상태 업데이트
+                        const updatedEmail =
+                            sessionStorage.getItem('useremail') ?? '';
+                        const kakaoLogin = !updatedEmail.includes('@');
+
+                        setUserInfo({
+                            username:
+                                sessionStorage.getItem('username') ??
+                                '방문자님',
+                            userid: kakaoLogin ? '카카오 로그인' : updatedEmail,
+                        });
+                    }}
+                />
+            )}
+
+            <div className="whitebox">
+                <h2>마이페이지</h2>
+                <div className="mypageContentsArea">
+                    {/* 사용자 정보 */}
+                    <div className="userArea">
+                        <div className="topArea">
+                            <h3>내 정보</h3>
+                            <button onClick={update} disabled={isKakaoLogin}>
+                                개인정보 수정
+                                <img src="/img/edit-01.png" alt="수정 아이콘" />
+                            </button>
+                        </div>
+
+                        <div className="userInfo">
+                            <div className="item">
+                                <div className="item-label">이름</div>
+                                <div className="item-value">
+                                    {userInfo.username}
+                                </div>
+                            </div>
+                            <div className="item">
+                                <div className="item-label">이메일</div>
+                                <div className="item-value">
+                                    {userInfo.userid}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    {/* 애기 정보 */}
+                    {babyInfo.length > 0 ? (
+                        <BabyInfo babyInfo={babyInfo} />
+                    ) : (
+                        <div className="w-full h-full flex justify-center items-center bg-gray-1 rounded-3xl">
+                            <div className="flex flex-col items-center gap-6 ">
+                                <img src="/img/Ggoggo-002.png" />
+                                <div className="text-xl font-md text-gray-6">
+                                    등록된 아이가 없습니다!
+                                </div>
+
+                                <button
+                                    onClick={() => setBabyPlus(true)}
+                                    className="button button-black button-lg"
+                                >
+                                    아이 등록하기
+                                    <img
+                                        src="../img/icons/i-chevron-right-s28.svg"
+                                        alt="바로가기 아이콘"
+                                    />
+                                </button>
+                            </div>
+                        </div>
+                    )}
+                </div>
             </div>
-          )}
-        </div>
-      </div>
-    </main>
-  );
+        </main>
+    );
 }
